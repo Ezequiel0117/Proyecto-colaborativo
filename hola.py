c234c0d34e2079e94b4ed280a4d9717f873b982e
isuarezt01 <isuarezt@unemi.edu.ec>
2025-06-05 00:41:04 -0500

    Agregue una funcion de despedida

--- hola.py
+++ hola.py
@@ -1,2 +1,22 @@
 def sealudar():
     print("Hola, Ezequiel!")
+    
+def despedida():
+    print("Adios, Ezequiel!")
+    
+    
+# git checkout -b rama-integrante2
+# xYugi — ayer a las 0:23
+# fatal: not a git repository (or any of the parent directories): .git
+# DARIO@DESKTOP-R8CINGO MINGW64 ~
+# $ git checkout -b rama-ivan
+# fatal: not a git repository (or any of the parent directories): .git
+# iNero0117 — ayer a las 0:28
+# def saludar():
+#     print("Hola desde Integrante 1")
+# git add hola.py
+# git commit -m "Añade función saludar en hola.py"
+# git push origin rama-integrante2
+# iNero0117 — ayer a las 0:41
+# git checkout main
+# git pull origin main
